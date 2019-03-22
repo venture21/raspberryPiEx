5896bb99b1a2a3f7a08e8176622f27aa314c2a0b
venture21 <park0122@gmail.com>
2019-03-22 15:32:07 +0900

    update sw_led.c
    update sw_led.c

--- sw_led.c
+++ sw_led.c
@@ -25,64 +25,55 @@ int main()
 	wiringPiSetup();
 	char val1, val2;
 
-
 	pinAssign();
 
+	// SW1이 눌리면 START
+	while(digitalRead(SW1));
 
 	if ((gettimeofday(&start_tv, NULL)) == -1)
 	{
 		perror("gettimeofday() call error");
 		return -1;
 	}
-	printf("Start Time");
-	printf("tv_sec=%ld,", start_tv.tv_sec);
-	printf("tv_usec=%ld\n", start_tv.tv_usec);
 
-	sleep(1);
-
-	if ((gettimeofday(&stop_tv, NULL)) == -1)
-	{
-		perror("gettimeofday() call error");
-		return -1;
-	}
-
-	printf("Stop Time");
-	printf("tv_sec=%ld,", stop_tv.tv_sec);
-	printf("tv_usec=%ld\n", stop_tv.tv_usec);
-
-	if (start_tv.tv_usec > stop_tv.tv_usec)
-	{
-		stop_tv.tv_sec--;
-		diff_tv.tv_usec = 1000000 + stop_tv.tv_usec - start_tv.tv_usec;
-		diff_tv.tv_sec = stop_tv.tv_sec - start_tv.tv_sec;
-	}
-	else
-	{
-		diff_tv.tv_usec = stop_tv.tv_usec - start_tv.tv_usec;
-		diff_tv.tv_sec = stop_tv.tv_sec - start_tv.tv_sec;
-	}
-
-	printf("DIFF Time");
-	printf("tv_sec=%ld,", diff_tv.tv_sec);
-	printf("tv_usec=%ld\n", diff_tv.tv_usec);
-
-	/*
-	printf("START\r");
-	fflush(stdout);
-	sleep(1);
-	printf("1\r");
-	fflush(stdout);
-	sleep(1);
-	printf("2\r");
-	fflush(stdout);
-	sleep(1);
-	printf("STOP\r");
-	fflush(stdout);
-	*/
-	/*
 	while (1)
 	{
-		val1 = digitalRead(SW1);
+		/*
+printf("Start Time");
+printf("tv_sec=%ld,", start_tv.tv_sec);
+printf("tv_usec=%ld\n", start_tv.tv_usec);
+*/
+		usleep(10000);
+
+		if ((gettimeofday(&stop_tv, NULL)) == -1)
+		{
+			perror("gettimeofday() call error");
+			return -1;
+		}
+		/*
+		printf("Stop Time");
+		printf("tv_sec=%ld,", stop_tv.tv_sec);
+		printf("tv_usec=%ld\n", stop_tv.tv_usec);
+		*/
+		if (start_tv.tv_usec > stop_tv.tv_usec)
+		{
+			stop_tv.tv_sec--;
+			diff_tv.tv_usec = 1000000 + stop_tv.tv_usec - start_tv.tv_usec;
+			diff_tv.tv_sec = stop_tv.tv_sec - start_tv.tv_sec;
+		}
+		else
+		{
+			diff_tv.tv_usec = stop_tv.tv_usec - start_tv.tv_usec;
+			diff_tv.tv_sec = stop_tv.tv_sec - start_tv.tv_sec;
+		}
+
+		//printf("DIFF Time");
+		printf("Time=%ld.", diff_tv.tv_sec);
+		printf("%ld\r", diff_tv.tv_usec);
+	}
+	/*
+
+
 		val2 = digitalRead(SW2);
 		if (val1 && val2)//check if the button is pressed, if yes, turn on the LED
 			digitalWrite(2, LOW);
