27f205506ca40664d47f4b53d411e510f2fe04b1
venture21 <park0122@gmail.com>
2019-04-08 17:49:03 +0900

    update turnLed.c
    update turnLed.c

--- DD/ex0/turnLed.c
+++ DD/ex0/turnLed.c
@@ -12,14 +12,15 @@
 
 //GPIO Function Select Register 0 ~ 5
 #define GPIO_IN(g)  (*(gpio+((g)/10)) &=~(7<<(((g)%10)*3)))
-#define GPIO_OUT(g)  (*(gpio+((g)/10)) |=(1<<(((g)%10)*3)))
+#define GPIO_OUT(g) {(*(gpio+((g)/10))&=~(7<<(((g)%10)*3))); \
+		     (*(gpio+((g)/10)) |=(1<<(((g)%10)*3)));}
 
 //GPIO Pin Output Set 0 / Clr 0
 //                      addr     value 
 #define GPIO_SET(g)  (*(gpio+7)=(1<<g))
 #define GPIO_CLR(g)  (*(gpio+10)=(1<<g))
 
-#define GPIO_GET(g)  (*(gpio+13)&1<<g))
+#define GPIO_GET(g)  (*(gpio+13)&1<<g)
 #define GPIO_SIZE 0xB4
 
 volatile unsigned int *gpio;
