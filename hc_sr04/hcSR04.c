afd317b8e5780246e71efa4390f6300c987d45eb
venture21 <park0122@gmail.com>
2019-04-18 13:41:22 +0900

    update hcSR04.c

--- hc_sr04/hcSR04.c
+++ hc_sr04/hcSR04.c
@@ -1,13 +1,35 @@
 #include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>  //gettimeofday()
+#include <unistd.h>	   //gettimeofday()
+
+#define trig 4
+#define echo 5
+
+void disp_runtime(struct timeval UTCtime_s, struct timeval UTCtime_e)
+{
+	struct timeval UTCtime_r;
+	if ((UTCtime_e.tv_usec - UTCtime_s.tv_usec) < 0)
+	{
+		UTCtime_r.tv_sec = UTCtime_e.tv_sec - UTCtime_s.tv_sec - 1;
+		UTCtime_r.tv_usec = 1000000 - UTCtime_e.tv_usec - UTCtime_s.tv_usec;
+	}
+	else
+	{
+		UTCtime_r.tv_sec = UTCtime_e.tv_sec - UTCtime_s.tv_sec;
+		UTCtime_r.tv_usec = UTCtime_e.tv_usec - UTCtime_s.tv_usec;
+	}
+	printf("runtime : %ld sec %ld\n", UTCtime_r.tv_sec, UTCtime_r.tv_usec);
+}
+
 
 int main(int argc, char *argv[])
 {
-	int trig = 23;
-	int echo = 24;
 	int startTime, endTime;
 	float distance;
+	int flag = 0;
+	struct timeval UTCtime_t1, UTCtime_t2;
 
 	// wiringPiSetup이 실패할 경우 종료 
 	if (wiringPiSetup() == -1)
@@ -20,6 +42,16 @@ int main(int argc, char *argv[])
 		// Trig신호 생성 (10us)
 		digitalWrite(trig, LOW);
 		delay(500);
+
+		if (!flag)
+		{	//trigger를 입력하는 시점에서 시간 저정
+			gettimeofday(&UTCtime_t1, NULL); // UTC 현재 시간 구하기(마이크로초까지)
+		}
+		else
+		{
+			gettimeofday(&UTCtime_t2, NULL); // UTC 현재 시간 구하기(마이크로초까지)
+			disp_runtime(UTCtime_t1, UTCtime_t2);
+		}
 		digitalWrite(trig, HIGH);
 		delayMicroseconds(10);
 		digitalWrite(trig, LOW);
