e3cf62adf1218e901fee790dc42c87407f5cfa66
venture21 <park0122@gmail.com>
2019-04-18 13:57:14 +0900

    update hcSR04.c

--- hc_sr04/hcSR04.c
+++ hc_sr04/hcSR04.c
@@ -7,9 +7,10 @@
 #define trig 4
 #define echo 5
 
+struct timeval UTCtime_r;
+
 void disp_runtime(struct timeval UTCtime_s, struct timeval UTCtime_e)
 {
-	struct timeval UTCtime_r;
 	if ((UTCtime_e.tv_usec - UTCtime_s.tv_usec) < 0)
 	{
 		UTCtime_r.tv_sec = UTCtime_e.tv_sec - UTCtime_s.tv_sec - 1;
@@ -20,15 +21,17 @@ void disp_runtime(struct timeval UTCtime_s, struct timeval UTCtime_e)
 		UTCtime_r.tv_sec = UTCtime_e.tv_sec - UTCtime_s.tv_sec;
 		UTCtime_r.tv_usec = UTCtime_e.tv_usec - UTCtime_s.tv_usec;
 	}
-	printf("runtime : %ld sec %ld\n", UTCtime_r.tv_sec, UTCtime_r.tv_usec);
+
 }
 
 
 int main(int argc, char *argv[])
 {
 	int startTime, endTime;
-	float distance;
+	float distance, before_distance, delta_s;
+	float velocity;
 	int flag = 0;
+	int count=0;
 	struct timeval UTCtime_t1, UTCtime_t2;
 
 	// wiringPiSetup이 실패할 경우 종료 
@@ -43,11 +46,12 @@ int main(int argc, char *argv[])
 		digitalWrite(trig, LOW);
 		delay(500);
 
-		if (!flag)
+		if (!flag && count>0)
 		{	//trigger를 입력하는 시점에서 시간 저정
 			gettimeofday(&UTCtime_t1, NULL); // UTC 현재 시간 구하기(마이크로초까지)
+			disp_runtime(UTCtime_t2, UTCtime_t1);
 		}
-		else
+		else if(flag && count>0)
 		{
 			gettimeofday(&UTCtime_t2, NULL); // UTC 현재 시간 구하기(마이크로초까지)
 			disp_runtime(UTCtime_t1, UTCtime_t2);
@@ -63,9 +67,23 @@ int main(int argc, char *argv[])
 		while (digitalRead(echo) == 1);
 		// Echo back이 '0'이면 종료시간 기록 
 		endTime = micros();
+
+		if (count > 0)
+			printf("runtime : %ld sec %ld\n", UTCtime_r.tv_sec, UTCtime_r.tv_usec);
+
 		// 거리 계산 공식
+		before_distance = distance;
 		distance = (endTime - startTime) / 58;
 		printf("distance %.2f cm\n", distance);
+
+		if (distance > before_distance)
+			delta_s = distance - before_distance;
+		else
+			delta_s = before_distance - distance;
+
+		velocity = (delta_s)/ (UTCtime_r.tv_usec * 10000);  // unit : m/s
+		printf("velocity %.2f cm\n", velocity);
+		count++;
 	}
 	return 0;
 }
