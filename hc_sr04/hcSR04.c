1dae4947239ce4d1faf16b3d7618ffb8db1623df
venture21 <park0122@gmail.com>
2019-04-18 14:17:02 +0900

    update hcSR04.c

--- hc_sr04/hcSR04.c
+++ hc_sr04/hcSR04.c
@@ -14,7 +14,7 @@ void disp_runtime(struct timeval UTCtime_s, struct timeval UTCtime_e)
 	if ((UTCtime_e.tv_usec - UTCtime_s.tv_usec) < 0)
 	{
 		UTCtime_r.tv_sec = UTCtime_e.tv_sec - UTCtime_s.tv_sec - 1;
-		UTCtime_r.tv_usec = 1000000 - UTCtime_e.tv_usec - UTCtime_s.tv_usec;
+		UTCtime_r.tv_usec = 1000000 + UTCtime_e.tv_usec - UTCtime_s.tv_usec;
 	}
 	else
 	{
@@ -50,11 +50,13 @@ int main(int argc, char *argv[])
 		{	//trigger를 입력하는 시점에서 시간 저정
 			gettimeofday(&UTCtime_t1, NULL); // UTC 현재 시간 구하기(마이크로초까지)
 			disp_runtime(UTCtime_t2, UTCtime_t1);
+			flag = 1;
 		}
 		else if(flag && count>0)
 		{
 			gettimeofday(&UTCtime_t2, NULL); // UTC 현재 시간 구하기(마이크로초까지)
 			disp_runtime(UTCtime_t1, UTCtime_t2);
+			flag = 0;
 		}
 		digitalWrite(trig, HIGH);
 		delayMicroseconds(10);
@@ -80,7 +82,7 @@ int main(int argc, char *argv[])
 			delta_s = distance - before_distance;
 		else
 			delta_s = before_distance - distance;
-
+		printf("delta_s %f \n", delta_s);
 		velocity = (delta_s*10000)/ (UTCtime_r.tv_usec);  // unit : m/s
 		printf("velocity %f m/s\n", velocity);
 		count++;
