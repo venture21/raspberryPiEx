f64f5318b0a9f5e172445617845179729075fe97
venture21 <park0122@gmail.com>
2019-04-23 11:40:39 +0900

    update helloServer.c

--- socket/helloServer.c
+++ socket/helloServer.c
@@ -39,24 +39,28 @@ int main(int argc, char* argv[])
 	//STEP 2. 접속할 서버의 IP주소, 포트번호, 프로토콜을 정의
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_addr.sin_port = htons(atoi(argv[2]));
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
 
-	//STEP 3. 접속 요청
-	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-	{
-		error_handling("connect() error!");
-	}
+	//STEP 3. BIND
+	if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+		error_handling("bind() error");
+		
+	//STEP 4. listen() 5의 의미는 동시접속을 처리할 수 있는 버퍼의 크기 
+	if(listen(serv_sock,5)==-1)
+		error_handling("listen() error");
 
-	// STEP 4. 데이터 수신
-	str_len = read(sock, message, sizeof(message) - 1);
-	if (str_len == -1)
-		error_handling("read() error!");
+	clnt_addr_size = sizeof(clnt_addr);
+	clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+	if(clnt_sock==-1)
+		error_handling("accept() error");
 
-	printf("Message from server: %s\n", message);
+	//STEP 5. 데이터 송신
+	write(clnt_sock, message, sizeof(message));
 
-	// STEP 5. 소켓 종료
-	close(sock);
+	// STEP 6. 소켓 종료
+	close(clnt_sock);
+	close(serv_sock);
 
 	return 0;
 }
